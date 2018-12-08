06ac8e415d2c706363fde67353b8ac9b79772b83
Matthew Femia <matthewfemia@matthews-macbook-air.local>
2018-12-08 02:01:23 -0500

    Initial Commit

---
+++ CoffeeCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeCounter
+//
+//  Created by Matthew Femia on 12/8/18.
+//  Copyright © 2018 Matthew Femia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
