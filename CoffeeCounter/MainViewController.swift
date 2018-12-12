26ca818d4f35f02dd9199ae9813a480480a25a15
Matt Femia <mattfemia1@gmail.com>
2018-12-12 01:08:33 -0500

    Added some functionality to mapkit -- now locates user and displays location on the MainViewController map

--- CoffeeCounter/MainViewController.swift
+++ CoffeeCounter/MainViewController.swift
@@ -9,22 +9,64 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import CoreLocation
+import MapKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // Representing the map itself on the MainViewController
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // Assign the location manager to a constant to be used
+    let locationManager: CLLocationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+        
+        if
+            CLLocationManager.locationServicesEnabled() == true {
+                if CLLocationManager.authorizationStatus() == .restricted ||
+                    CLLocationManager.authorizationStatus() == .denied ||
+                    CLLocationManager.authorizationStatus() == .notDetermined {
+                    
+                    // Requests location services be used when the app is in use
+                    locationManager.requestWhenInUseAuthorization()
+            }
+            
+            // Used to represent location accuracy level in meters (Read docs)
+            locationManager.desiredAccuracy = 1.0
+            locationManager.delegate = self
+            
+            // Location manager will give us updates about location
+            locationManager.startUpdatingLocation()
+            locationManager.distanceFilter = 5
+            
+        } else {
+            print("Please turn on location services")
+        }
     }
+    
+    // CLLocationManager Delegates
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+        self.mapView.setRegion(currRegion, animated: true)
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Current location could not be accessed")
+    }
+    
+    
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        if let user = Auth.auth().currentUser {
-            self.performSegue(withIdentifier: "loginToInitial", sender: self)
         }
-    }
     
     // Make a counter
         // TODO
@@ -38,7 +80,7 @@ class MainViewController: UIViewController {
         
         performSegue(withIdentifier: "mainToInitial", sender: self)
     }
-    /* MAKE USER PROFILE
+    /* MAKE USER PROFILE */
         // Ranking for how many coffees per day? week? ever?
         // Ability to add friends
         // Capability of sending friends your free coffee with message attached
